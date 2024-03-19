a39d87e11c8192d0cd8ac9b27506b6ea8385bc80
Matt Whitworth <matt@puretype.ai>
2024-03-19 17:06:24 +0000

    start proof of concept for authentication

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,24 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log('Extension started');
+const RETURN_URI = `${vscode.env.uriScheme}://puretype.puretype`;
+
+class AuthenticationUriHandler implements vscode.UriHandler {
+  handleUri(uri: vscode.Uri): vscode.ProviderResult<void> {
+    vscode.window.showInformationMessage(uri.query);
+  }
+}
+
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	const authUriHandler = new AuthenticationUriHandler();
+
+	context.subscriptions.push(vscode.window.registerUriHandler(authUriHandler));
+
+	const actionToTake = await vscode.window.showInformationMessage(`Authenticate with PureType`, { modal: false }, { title: 'Login' });
+
+	console.log(actionToTake);
+
+	const uri = await vscode.env.asExternalUri(vscode.Uri.parse('http://localhost:4000/user/login'));
+	await vscode.env.openExternal(uri);
 }
 
 export function deactivate() {}
