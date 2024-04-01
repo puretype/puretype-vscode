e3d889519cf12ce937b5869edd2430978c9b9b3d
Matt Whitworth <matt@puretype.ai>
2024-04-01 18:44:32 +0100

    add references to analyzer from elsewhere

--- src/hover.ts
+++ src/hover.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
+import { Analyzer } from "./analyzer";
 
 export class PureTypeHoverProvider implements vscode.HoverProvider {
+  constructor(private analyzer: Analyzer) {}
+
   provideHover(
     document: vscode.TextDocument,
     position: vscode.Position,
