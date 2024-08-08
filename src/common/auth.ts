080519ff22261e5838ca80cd0b5830ee77984352
Matt Whitworth <matt@puretype.ai>
2024-08-08 14:00:32 -0400

    expose interface for access token state change

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -7,15 +7,24 @@ export default class Auth {
 
   storeAccessToken(accessToken: string): void {
     this.secrets.store("accessToken", accessToken);
+    this.accessTokenSet.fire();
   }
 
   clearAccessToken(): void {
     this.secrets.delete("accessToken");
+    this.accessTokenClear.fire();
   }
 
   getAccessToken(): Thenable<string | undefined> {
     return this.secrets.get("accessToken");
   }
+
+  private accessTokenSet: vscode.EventEmitter<void> = new vscode.EventEmitter();
+  private accessTokenClear: vscode.EventEmitter<void> =
+    new vscode.EventEmitter();
+
+  readonly onAccessTokenSet: vscode.Event<void> = this.accessTokenSet.event;
+  readonly onAccessTokenClear: vscode.Event<void> = this.accessTokenClear.event;
 
   private secrets: vscode.SecretStorage;
 }
